e0c2ec0dfcc63fd8b09b98037d6becbe3a845b2c
Youngjoon Kim <acorn421@gmail.com>
2026-04-22 17:22:33 +0000

    fix: increase docker commit timeout
    Signed-off-by: Youngjoon Kim <acorn421@gmail.com>

--- oss_crs/src/crs_compose.py
+++ oss_crs/src/crs_compose.py
@@ -292,7 +292,7 @@ class CRSCompose:
         Returns:
             True if all snapshots committed successfully, False otherwise (with cleanup).
         """
-        client = docker.from_env(timeout=1800)
+        client = docker.from_env(timeout=3600)
         committed_tags: list[str] = []
         target_env = target.get_target_env()
         oss_fuzz_env = {k: target_env[v] for k, v in OSS_FUZZ_TARGET_ENV.items()}
